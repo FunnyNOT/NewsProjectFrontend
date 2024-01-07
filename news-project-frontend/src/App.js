8516e39bba51f8b27a841504bc970bdd29b93dba
Leonidas Oikonomou <leonidas@alumnireach.com>
2024-01-07 13:03:52 +0200

    starter frontend app

---
+++ news-project-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ApiDataDisplay from './ApiDataDisplay';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiDataDisplay />
+    </div>
+  );
+}
+
+export default App;
