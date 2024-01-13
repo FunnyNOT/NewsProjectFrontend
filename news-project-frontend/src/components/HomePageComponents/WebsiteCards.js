466794c0ac0bc8736b9af7e334dce433401abe10
Leonidas Oikonomou <leonidas@alumnireach.com>
2024-01-13 14:45:43 +0200

    prettier formatting

--- news-project-frontend/src/components/HomePageComponents/WebsiteCards.js
+++ news-project-frontend/src/components/HomePageComponents/WebsiteCards.js
@@ -1,87 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, Divider } from '@mui/material';
-import { fetchData } from '../../global_functions/ApiDataDisplay';
-
+import React, { useState, useEffect } from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { Button, CardActionArea, Divider } from '@mui/material'
+import { fetchData } from '../../global_functions/ApiDataDisplay'
 
 import { fetchData } from '../../global_functions/ApiDataDisplay'
 
-
 export default function WebsiteCards() {
+  const [data, setData] = useState(null)
 
-    const [data, setData] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      const result = await fetchData()
+      setData(result)
+    }
 
-    useEffect(() => {
-        const getData = async () => {
-            const result = await fetchData();
-            setData(result);
-        };
+    getData()
+  }, [])
+  return (
+    <>
+      {data &&
+        data.map((item, index) => (
+          <>
+            <Card
+              style={{
+                display: 'flex',
+                margin: '5px',
+                backgroundColor: '#242b2c',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              variant='outlined'
+            >
+              <CardActionArea href={item.website_link} style={{ display: 'flex', width: '75%' }}>
+                <CardMedia
+                  component='img'
+                  height='180'
+                  image={`${process.env.PUBLIC_URL}/images/defaultNewsImage.png`}
+                  alt='green iguana'
+                  style={{ width: '25%', borderRadius: '8px' }}
+                />
 
-        getData();
-    }, []);
-    return (
-        <>
-            {data && data.map((item, index) => (
-                <>
-                    <Card style={{  
-                        display: 'flex',
-                        margin: '5px', 
-                        backgroundColor: '#242b2c', 
-                        width: '100%',
-                        justifyContent: 'center',
-                        alignItems: 'center',}}
-                        variant="outlined" >
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='div' style={{ color: '#da292f' }}>
+                    {item.website_name}
+                  </Typography>
 
-                        <CardActionArea 
-                            href={item.website_link} 
-                            style={{    display: 'flex', 
-                                        width: '75%' }}>
+                  <Typography variant='caption' color='text.secondary' style={{ color: '#f9f9f9' }}>
+                    {item.website_description.substring(0, 200) + '...'}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
 
-                            <CardMedia
-                                component="img"
-                                height="180"
-                                image={`${process.env.PUBLIC_URL}/images/defaultNewsImage.png`}
-                                alt="green iguana"
-                                style={{ width: '25%', borderRadius: '8px' }}
-                            />
+              <CardContent
+                style={{ flex: '1', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end' }}
+              >
+                <Button variant='contained' color='primary'>
+                  Visit Website
+                </Button>
+              </CardContent>
+            </Card>
 
-                            <CardContent>
-
-                                <Typography gutterBottom variant="h5" component="div" style={{ color: '#da292f' }}>
-                                    {item.website_name}
-                                </Typography>
-                                
-                                <Typography variant="caption" color="text.secondary" style={{ color: '#f9f9f9' }}>
-                                    {item.website_description.substring(0, 200) + '...'}
-                                </Typography>
-                            
-                            </CardContent>
-
-                        </CardActionArea>
-
-                        <CardContent style={{   flex: '1', 
-                                                display: 'flex', 
-                                                flexDirection: 'column', 
-                                                justifyContent: 'center', 
-                                                alignItems: 'flex-end' }}>
-                                        
-                            <Button variant="contained" color="primary">
-                                Visit
-                                Website
-                            </Button>
-
-                        </CardContent>
-                    
-                    </Card>
-
-                    <Divider style={{ color: '#f9f9f9', borderColor: '#f9f9f9' }} />
-                </>
-            ))}
-        </>
-    );
+            <Divider style={{ color: '#f9f9f9', borderColor: '#f9f9f9' }} />
+          </>
+        ))}
+    </>
+  )
 }
 
 export { WebsiteCards }
--- news-project-frontend/src/components/Homepage.js
+++ news-project-frontend/src/components/Homepage.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import { WebsiteCards } from './HomePageComponents/WebsiteCards';
-import { DrawerAppBar } from './globalComponents/Header';
-import Box from '@mui/system/Box';
-import { styled } from '@mui/material';
+import React from 'react'
+import { WebsiteCards } from './HomePageComponents/WebsiteCards'
+import { DrawerAppBar } from './globalComponents/Header'
+import Box from '@mui/system/Box'
+import { styled } from '@mui/material'
 
 const StyledHomePage = styled('div')({
-    backgroundColor: '#242b2c',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%'
-});
+  backgroundColor: '#242b2c',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%'
+})
 
 const Homepage = () => {
+  return (
+    <StyledHomePage>
+      <DrawerAppBar />
 
-    return (
-        <StyledHomePage>
-
-            <DrawerAppBar  />
-
-            <Box component="section" sx={{ p: 10 }}>
-                <WebsiteCards /> 
-            </Box>
-            
-        </StyledHomePage>
-    );
+      <Box component='section' sx={{ p: 10 }}>
+        <WebsiteCards />
+      </Box>
+    </StyledHomePage>
+  )
 }
 
-
-export default Homepage;
+export default Homepage
