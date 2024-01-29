002b9459fba5cced50f8853bfde79e5ac8241e04
gusalx91 <gusalx91@gmail.com>
2024-01-29 17:48:49 +0200

    added search in website profile

---
+++ news-project-frontend/src/global_functions/CreatePseudoId.js
@@ -0,0 +1,18 @@
+// Create pseudoId to use in Website Cards
+
+function generateRandomNumber(length) {
+  const min = Math.pow(10, length - 1)
+  const max = Math.pow(10, length) - 1
+
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+function createPseudoId(websiteId) {
+  const firstNumber = generateRandomNumber(3).toString()
+  const secondNumber = generateRandomNumber(3).toString()
+  const websiteIdString = websiteId.toString()
+  const combinedString = firstNumber + websiteIdString + secondNumber
+  const combinedNumber = parseInt(combinedString, 10)
+  return combinedNumber
+}
+
+export { createPseudoId }
